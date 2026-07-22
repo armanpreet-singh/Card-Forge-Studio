14af41e2a45b1df7823d2328723601828361ce95
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-22 12:23:49 +0530

    add print functionality with bindPrint event listener

---
+++ js/ui/print.js
@@ -0,0 +1,6 @@
+/* ================================================================
+   PRINT
+================================================================ */
+export function bindPrint() {
+  document.getElementById('btnPrint').addEventListener('click', () => window.print());
+}
